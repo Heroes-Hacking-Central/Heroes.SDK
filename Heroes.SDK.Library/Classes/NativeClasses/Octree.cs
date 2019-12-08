2a8d5cb9c32e99578e00b7a7913f3d025538d029
Sewer56 <seweratwork@gmail.com>
2019-12-08 17:35:11 +0000

    Octree: Minor cleanup

--- Heroes.SDK.Library/Classes/NativeClasses/Octree.cs
+++ Heroes.SDK.Library/Classes/NativeClasses/Octree.cs
@@ -4,19 +4,18 @@ using Heroes.SDK.Definitions.Structures.Collision.Stage;
 using Reloaded.Hooks;
 using Reloaded.Hooks.Definitions;
 using Reloaded.Hooks.Definitions.X86;
+using static Reloaded.Hooks.Definitions.X86.FunctionAttribute;
 
 namespace Heroes.SDK.Classes.NativeClasses
 {
     /// <summary>
     /// Note: This is actually a quadtree.
     /// </summary>
+    [StructLayout(LayoutKind.Explicit)]
     [Equals(DoNotAddEqualityOperators =true)]
-    public struct Octree
+    public unsafe struct Octree
     {
         // TODO: In-memory parser of Collision Files, just like there is a parser of ONE files.
-
-        /* Class Members */
-        public QuadtreeFileHeader Header;
 
         /* Function Declarations */
         public static IFunction<Native_Destructor> Fun_Destructor = SDK.ReloadedHooks.CreateFunction<Native_Destructor>(0x0042D1E0);
@@ -35,7 +34,7 @@ namespace Heroes.SDK.Classes.NativeClasses
         /// </summary>*
         /// <param name="octreeFilePtr">Address of the Octree file in memory.</param>
         [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
-        [Function(new[] { FunctionAttribute.Register.esi }, FunctionAttribute.Register.eax, FunctionAttribute.StackCleanup.None)]
+        [Function(new[] { Register.esi }, Register.eax, StackCleanup.None)]
         public delegate int Native_Destructor(ref Octree octreeFilePtr);
     }
 }
