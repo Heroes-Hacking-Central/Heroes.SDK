c6022d805e6603303d06521cb045c9e3c04c3682
Sewer56 <seweratwork@gmail.com>
2019-12-08 10:59:34 +0000

    Add function to VictoryTracker to get number of victories for player.

--- Heroes.SDK.Library/Definitions/Structures/State/VictoryTracker.cs
+++ Heroes.SDK.Library/Definitions/Structures/State/VictoryTracker.cs
@@ -1,4 +1,7 @@
-﻿namespace Heroes.SDK.Definitions.Structures.State
+﻿using System;
+using Heroes.SDK.Definitions.Enums.Custom;
+
+namespace Heroes.SDK.Definitions.Structures.State
 {
     /// <summary>
     /// Structure that tracks the individual victories for each of the four players.
@@ -10,5 +13,26 @@
         public byte PlayerTwo;
         public byte PlayerThree;
         public byte PlayerFour;
+
+        /// <summary>
+        /// Returns the number of victories for a specific player.
+        /// </summary>
+        /// <param name="player">The player to get victories for.</param>
+        public byte GetNumberOfVictories(Players player)
+        {
+            switch (player)
+            {
+                case Players.One:
+                    return PlayerOne;
+                case Players.Two:
+                    return PlayerTwo;
+                case Players.Three:
+                    return PlayerThree;
+                case Players.Four:
+                    return PlayerFour;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(player), player, null);
+            }
+        }
     }
 }
