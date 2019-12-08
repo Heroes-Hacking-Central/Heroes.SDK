4984fe4b3857127aa83e3a3bac14b414eae475ad
Sewer56 <seweratwork@gmail.com>
2019-12-08 19:36:25 +0000

    [Untested] Add Spline Export Classes

---
+++ Heroes.SDK.Library/Parsers/Custom/SplineFile.cs
@@ -0,0 +1,15 @@
+﻿using SonicHeroes.Utils.StageInjector.Common.Utilities;
+
+namespace SonicHeroes.Utils.StageInjector.Common
+{
+    public class SplineFile : JsonSerializable<SplineFile>
+    {
+        public ManagedSpline[] Splines { get; set; }
+
+        public SplineFile() { }
+        public SplineFile(ManagedSpline[] splines)
+        {
+            Splines = splines;
+        }
+    }
+}
---
+++ Heroes.SDK.Library/Utilities/Misc/JsonSerializable.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace SonicHeroes.Utils.StageInjector.Common.Utilities
+{
+    public abstract class JsonSerializable<TType> where TType : new()
+    {
+        private static JsonSerializerOptions _options = new JsonSerializerOptions()
+        {
+            Converters = { new JsonStringEnumConverter() },
+            WriteIndented = true
+        };
+
+        public static TType FromPath(string filePath)
+        {
+            if (File.Exists(filePath))
+            {
+                string jsonFile = File.ReadAllText(filePath);
+                return JsonSerializer.Deserialize<TType>(jsonFile, _options);
+            }
+            else
+            {
+                var newFile = new TType();
+                ToPath(newFile, filePath);
+                return newFile;
+            }
+        }
+
+        public static void ToPath(TType config, string filePath)
+        {
+            string fullPath = Path.GetFullPath(filePath);
+            string directoryOfPath = Path.GetDirectoryName(fullPath);
+            if (!Directory.Exists(directoryOfPath))
+                Directory.CreateDirectory(directoryOfPath);
+
+            string jsonFile = JsonSerializer.Serialize(config, _options);
+            File.WriteAllText(fullPath, jsonFile);
+        }
+    }
+}
