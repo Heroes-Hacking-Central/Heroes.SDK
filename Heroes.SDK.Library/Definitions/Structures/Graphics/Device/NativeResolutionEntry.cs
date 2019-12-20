7158b6482fb4fa9038257effe658272b26944b88
Seweryn Szulgan <seweratwork@gmail.com>
2019-12-20 21:24:40 +0000

    Changed Entries to GetEntries, as property may cause issues compiling.

--- Heroes.SDK.Library/Definitions/Structures/Graphics/Device/NativeResolutionEntry.cs
+++ Heroes.SDK.Library/Definitions/Structures/Graphics/Device/NativeResolutionEntry.cs
@@ -1,15 +1,16 @@
-﻿using Reloaded.Memory.Pointers;
+﻿using System.Runtime.InteropServices;
+using Reloaded.Memory.Pointers;
 
 namespace Heroes.SDK.Definitions.Structures.Graphics.Device
 {
     /// <summary>
     /// Replicates the hardcoded resolution struct stored inside the Sonic Heroes executable.
     /// </summary>
-    [Equals(DoNotAddEqualityOperators =true)]
+    [Equals(DoNotAddEqualityOperators = true)]
     public struct NativeResolutionEntry
     {
         // TODO: No idea where to put this. Changing this has kinda no effect once the game starts running so /shrug
-        public static RefFixedArrayPtr<NativeResolutionEntry> Entries { get; } = new RefFixedArrayPtr<NativeResolutionEntry>(0x7C9290, 8);
+        public static RefFixedArrayPtr<NativeResolutionEntry> GetEntries() => new RefFixedArrayPtr<NativeResolutionEntry>(0x7C9290, 8);
 
         public int Width;
         public int Height;
